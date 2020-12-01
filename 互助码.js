891e7d12850660b7858fbdf166e709f2d648d95f
Xin <975203723@qq.com>
2020-12-01 17:56:29 +0800

    JD_互助码

---
+++ 互助码.js
@@ -0,0 +1,102 @@
+// 农场4位，萌宠5位，种豆3位
+// cron 秒分时日月周
+
+【东东农场jd_fruit】
+// zxg975203723
+554359d55fcc4a1fab5f17a946cbf338
+// 孑然一身曲一梦
+4625d3914d044bacbae685e9d60f14c8
+
+【东东萌宠jd_pet】
+// zxg975203723
+MTE1NDQ5MzYwMDAwMDAwMzkwMTMzNTk=
+// 孑然一身曲一梦
+MTAxODEyMjkxMDAwMDAwMDQwMDQ5ODU5
+
+【种豆得豆jd_plantBean】
+// zxg975203723
+mlrdw3aw26j3xtlnoc5rovsdcl364xjx4ctvrhq
+// 孑然一身曲一梦
+e7lhibzb3zek2volndkiirtldf2etf3cbfqgk5y
+
+【东东工厂】
+// zxg975203723
+P04z54XCjVWnYaS5m9cZ2X4jnwZkP19boOcCfg
+// 孑然一身曲一梦
+P04z54XCjVWnYaS5m9cZ2T4j3pCx8SL_QDb89E
+
+【京喜工厂】
+// zxg975203723
+7cgl2Xm_soDzanE7bzNVdA==
+// 孑然一身曲一梦
+vuPgt-FKtWwBSRuB_yxNJw==
+
+
+
+
+// 每个月1号，10号，20号凌晨2点清理一次数据库，清理后需重新点击链接提交互助码。
+
+/*——————————————————————————————东东农场——————————————————————————————*/
+
+// 【东东农场】api
+// zxg975203723
+http://api.turinglabs.net/api/v1/jd/farm/create/554359d55fcc4a1fab5f17a946cbf338/
+// 孑然一身曲一梦
+http://api.turinglabs.net/api/v1/jd/farm/create/4625d3914d044bacbae685e9d60f14c8/
+
+// 查看上车人数
+http://api.turinglabs.net/api/v1/jd/farm/count/
+
+
+/*——————————————————————————————东东萌宠——————————————————————————————*/
+
+// 【东东萌宠】api
+// zxg975203723
+http://api.turinglabs.net/api/v1/jd/pet/create/MTE1NDQ5MzYwMDAwMDAwMzkwMTMzNTk=/
+// 孑然一身曲一梦
+http://api.turinglabs.net/api/v1/jd/pet/create/MTAxODEyMjkxMDAwMDAwMDQwMDQ5ODU5/
+
+// 查看上车人数
+http://api.turinglabs.net/api/v1/jd/pet/count/
+
+
+/*——————————————————————————————种豆得豆——————————————————————————————*/
+
+// 【种豆得豆】api
+// zxg975203723
+http://api.turinglabs.net/api/v1/jd/bean/create/mlrdw3aw26j3xtlnoc5rovsdcl364xjx4ctvrhq/
+// 孑然一身曲一梦
+http://api.turinglabs.net/api/v1/jd/bean/create/e7lhibzb3zek2volndkiirtldf2etf3cbfqgk5y/
+
+// 查看上车人数
+http://api.turinglabs.net/api/v1/jd/bean/count/
+
+
+/*——————————————————————————————东东工厂——————————————————————————————*/
+
+// 【东东工厂】api
+// zxg975203723
+http://api.turinglabs.net/api/v1/jd/ddfactory/create/P04z54XCjVWnYaS5m9cZ2X4jnwZkP19boOcCfg/
+// 孑然一身曲一梦
+http://api.turinglabs.net/api/v1/jd/ddfactory/create/P04z54XCjVWnYaS5m9cZ2T4j3pCx8SL_QDb89E/
+
+// 查看上车人数
+http://api.turinglabs.net/api/v1/jd/ddfactory/count/
+
+
+/*——————————————————————————————京喜工厂——————————————————————————————*/
+
+// 【京喜工厂】api
+// zxg975203723
+http://api.turinglabs.net/api/v1/jd/jxfactory/create/7cgl2Xm_soDzanE7bzNVdA==/
+// 孑然一身曲一梦
+http://api.turinglabs.net/api/v1/jd/jxfactory/create/vuPgt-FKtWwBSRuB_yxNJw==/
+
+// 查看上车人数
+http://api.turinglabs.net/api/v1/jd/jxfactory/count/
+
+
+
+
+// 【查看数据库清空时间】
+http://api.turinglabs.net/api/v1/jd/cleantimeinfo/
